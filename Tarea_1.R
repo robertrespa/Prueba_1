d0c1d7d8970c94590d8a0cbd2dcd5c9e71c697f3
robertrespa <roberto.trespalacio@upr.edu>
2017-02-12 04:08:25 -0400

    Add files via upload

---
+++ Tarea_1.R
@@ -0,0 +1,39 @@
+# Tarea 1.
+# Solucione los siguientes problemas para entregarlos el día miércoles 15 de febrero en clase.
+
+################################################# Problemas ##################################################################
+
+# Problema 1. La Compañía de Turismo de Puerto Rico está interesado en saber de que país son los visitantes que
+#             llegan a Puerto Rico durante los meses de verano. Para esto se contrató al personal capacitado para 
+#             que tome una muestra al azar de 49 visitantes y se les preguntó su nacionalidad: 
+
+pais = c("EEUU", "EEUU", "EEUU", "EEUU", "Espana", "Espana", "Espana", "Espana",
+         "Espana", "EEUU", "EEUU", "EEUU", "EEUU", "EEUU", "Espana", "Venezuela",
+         "Espana", "Espana", "Venezuela", "Espana", "EEUU", "EEUU",  "Espana",
+         "Venezuela", "Dominica", "Dominica" , "Dominica", "Espana", "EEUU",
+         "Espana", "Espana", "EEUU", "EEUU", "EEUU", "EEUU", "EEUU", "Espana", 
+         "EEUU", "Espana", "Espana", "EEUU", "EEUU", "EEUU", "Venezuela", 
+         "Espana", "Venezuela", "Venezuela", "Venezuela", "Venezuela")
+
+
+# Para la muestra de paises, construya. 
+# a. Una tabla de distribución de frecuencias (Realice algunas interpretaciones de la tabla) (manualmente y en R)
+# b. Diagrama de Barras de frecuencias absolutas y relativas) (Realice algunas interpretaciones del gráfico) (manualmente y en R)
+# c. Diagrama de Pareto (Realice algunas interpretaciones del gráfico) (en R)
+# d. Pie Chart (Realice algunas interpretaciones del gráfico) (en R)
+
+
+##########################################################################################################################################
+# Probelma 2. Para los datos mtcars:
+
+# a. Con la variable qsec, construya una tabla de distribución de frecuencias. (en R)
+#    (nota: Usar la regla de Sturges para ver el número de clases óptimo)
+#    (Realice algunas interpretaciones de la tabla)
+
+# b. Con la variable qsec, construya un histograma de frecuencias absolutas relativas y acumuladas 
+#    (Realice algunas interpretaciones de cada gráfico) (en R)
+
+# c. Con la variable qsec, construya una ojiva (Realice algunas interpretaciones del gráfico) (en R)
+
+
+
